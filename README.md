316794434efe17c09da9bd62f64b0a2b0e001146
Jorge Martinez <jlmartinez@acropolismarketing.com>
2026-02-15 19:31:48 -0500

    Revival

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
+# Compiling and Running the Project (Ubuntu)
+
+This project is written in F# and uses the .NET SDK. To build and run it on Ubuntu:
+
+## Prerequisites
+
+- Install the .NET SDK (if not already installed):
+	- [Install .NET SDK](https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu)
+	- You can check your installation with:
+		```bash
+		dotnet --version
+		```
+
+## Build the Project
+
+From the project root directory:
+
+```bash
+dotnet build WingspanCOTB/WingspanCOTB.fsproj
+```
+
+## Run the Project
+
+```bash
+dotnet run --project WingspanCOTB/WingspanCOTB.fsproj
+```
+
+## Troubleshooting
+
+- If you encounter errors, ensure your .NET SDK is up to date.
+- Restore dependencies if needed:
+	```bash
+	dotnet restore WingspanCOTB/WingspanCOTB.fsproj
+	```
+
+---
 # wingspan-cotb
 An F# Representation &amp; Symbolic AI for the Wingspan video game's Champ of the Birds mode.
--- WingspanCOTB/Game.fs
+++ WingspanCOTB/Game.fs
@@ -6,34 +6,32 @@ module Game =
     open WingspanCOTB.StartingChoice
     open WingspanCOTB.BonusCard
 
-    type Config = {
-        StartingChoices : StartingChoice list
-        BonusCardChoices: IBonusCard list
-        BirdfeederSeries: FoodDie list
-    }
+    type Config =
+        { StartingChoices: StartingChoice list
+          BonusCardChoices: IBonusCard list
+          BirdfeederSeries: FoodDie list }
+
     type AutomaPrimary =
         | AutomaDrawCards
         | AutomaPlayBirds
         | AutomaLayEggs of int
-        | AutomaGainFood of FoodDie list 
-    type AutomaSecondary = 
+        | AutomaGainFood of FoodDie list
+
+    type AutomaSecondary =
         | AutomaAddToGoal
         | AutomaRemoveFromGoal
         | AutomaActivateAllPink
+
     type Action =
         | DrawCards
         | GainFood
         | PlayBird of Bird.Bird
         | LayEggs of Bird.Bird list
         | AutomaMove of AutomaPrimary * AutomaSecondary
-    type InProgressState = {
-        Round: int
-        Turn: int
-    }
-    type FinalState = {
-        Winner: IPlayer
-        Score: int
-    }
+
+    type InProgressState = { Round: int; Turn: int }
+    type FinalState = { Winner: IPlayer; Score: int }
+
     and Phase =
         | PickBirdsAndFood of StartingChoice list
         | PickBonusCards of IBonusCard list
@@ -41,16 +39,17 @@ module Game =
         | PickBirdPower
         | EndOfTurn
         | Final of FinalState
-    and Game = {
-        Phase: Phase
-        Config: Config
-        CurrentPlayer: IPlayer
-        OtherPlayer: IPlayer
-        Deck: Bird.Bird list
-        Tray: Bird.Bird list
-        Birdfeeder: FoodDie list
-    }
-    and IPlayer = 
+
+    and Game =
+        { Phase: Phase
+          Config: Config
+          CurrentPlayer: IPlayer
+          OtherPlayer: IPlayer
+          Deck: Bird.Bird list
+          Tray: Bird.Bird list
+          Birdfeeder: FoodDie list }
+
+    and IPlayer =
         abstract member Name: string
         abstract member PromptStartingChoices: Game * StartingChoice list -> StartingChoice list
         abstract member ApplyStartingChoices: StartingChoice list -> IPlayer
@@ -63,41 +62,47 @@ module Game =
     let rerollBirdfeeder game =
         let nextDice = List.take 5 game.Config.BirdfeederSeries
         let bfSeries = List.skip 5 game.Config.BirdfeederSeries
+
         { game with
             Birdfeeder = nextDice
-            Config = {game.Config with BirdfeederSeries = bfSeries}
-        }
+            Config =
+                { game.Config with
+                    BirdfeederSeries = bfSeries } }
 
     let refillTray game =
         let n = 3 - game.Tray.Length
+
         { game with
             Tray = (List.take n game.Deck) @ game.Tray
-            Deck = List.skip n game.Deck
-        }
-    
+            Deck = List.skip n game.Deck }
+
     let applyStartingChoices game choices =
-        { game with 
+        { game with
             CurrentPlayer = game.CurrentPlayer.ApplyStartingChoices(choices)
-            Phase = PickBonusCards(game.Config.BonusCardChoices)
-        }
+            Phase = PickBonusCards(game.Config.BonusCardChoices) }
 
     let applyStartingBonusCard game choice =
-        { game with 
+        { game with
             CurrentPlayer = game.CurrentPlayer.ApplyStartingBonusCard(choice)
-            Phase = PickAction({Round = 1; Turn = 0;})
-        } |> rerollBirdfeeder |> refillTray
+            Phase = PickAction({ Round = 1; Turn = 0 }) }
+        |> rerollBirdfeeder
+        |> refillTray
 
     let applyAction game action =
         // First we modify the player's state
-        let game = { game with CurrentPlayer = game.CurrentPlayer.ApplyAction(game, action) }
+        let game =
+            { game with
+                CurrentPlayer = game.CurrentPlayer.ApplyAction(game, action) }
         // TODO: here modify the game's state
-        let nextPhase = 
+        let nextPhase =
             match game.CurrentPlayer.CanUseBirdPower(game) with
             | true -> PickBirdPower
             | false -> EndOfTurn
+
         { game with Phase = nextPhase }
 
     let rec loop game =
+
         match game.Phase with
         | PickBirdsAndFood allChoices ->
             let choices = game.CurrentPlayer.PromptStartingChoices(game, allChoices)
@@ -108,6 +113,6 @@ module Game =
         | PickAction pa ->
             let action = game.CurrentPlayer.PromptAction(game)
             loop (applyAction game action)
-        | PickBirdPower _ -> raise (System.NotImplementedException())
-        | EndOfTurn _ -> raise (System.NotImplementedException())
+        | PickBirdPower -> raise (System.NotImplementedException())
+        | EndOfTurn -> raise (System.NotImplementedException())
         | Final f -> (f.Winner, f.Score)
